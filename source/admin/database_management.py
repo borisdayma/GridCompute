895f552a9516d87e038c7c43c2686020e35d5da4
Boris Dayma <boris.dayma@gmail.com>
2014-08-11 19:20:32 -0500

    Typo in license
    Replaced Foobar with GridCompute

--- source/admin/database_management.py
+++ source/admin/database_management.py
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with GridCompute.  If not, see <http://www.gnu.org/licenses/>.
 #
 # For any question, please contact Boris Dayma at boris.dayma@gmail.com
 
