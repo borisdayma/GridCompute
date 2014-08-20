beecd130c82b017bc862679be6911ffa3fce118f
Boris Dayma <boris.dayma@gmail.com>
2014-08-20 00:34:15 -0500

    Fix build of documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+from unittest.mock import MagicMock # to ignore modules that don't import well in RTD
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -24,6 +25,15 @@ sys.path.insert(0, os.path.abspath('../../source/admin'))
 sys.path.insert(0, os.path.abspath('../../template/Shared_Folder/Settings/Applications/Random Counter'))
 
 # -- General configuration ------------------------------------------------
+
+# Ignore modules that cannot be imported by RTD
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['module_name']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
